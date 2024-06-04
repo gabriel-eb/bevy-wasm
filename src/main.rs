9852729d69f9b3beafffad92205c06aa9f539a7d
gabriel-eb <gabo_espburg@hotmail.com>
2024-06-04 19:54:22 -0400

    add snake head component

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,15 @@
 use bevy::prelude::*;
 
+const SNAKE_HEAD_COLOR: Color = Color::rgb(0.7, 0.7, 0.7);
+#[warn(dead_code)]
+const SNAKE_BODY_COLOR: Color = Color::rgb(0.6, 0.6, 0.6);
+#[warn(dead_code)]
+const FOOD_COLOR: Color = Color::rgb(0.3, 0.8, 0.5);
+
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .add_systems(Startup, setup_camera)
+        .add_systems(Startup, (setup_camera, spawn_snake))
         .run();
 }
 
@@ -17,5 +23,25 @@ fn setup_camera(mut commands: Commands) {
             ..default()
         },
         MyCameraMaker,
+    ));
+}
+
+#[derive(Component)]
+struct SnakeHead;
+
+fn spawn_snake(mut commands: Commands) {
+    commands.spawn((
+        SpriteBundle {
+            sprite: Sprite {
+                color: SNAKE_HEAD_COLOR,
+                ..default()
+            },
+            transform: Transform {
+                scale: Vec3::new(10.0, 10.0, 10.0),
+                ..default()
+            },
+            ..default()
+        },
+        SnakeHead,
     ));
 }
