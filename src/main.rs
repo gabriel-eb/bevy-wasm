311898683cb3abbb76c0c5c109233f976d9d8bc3
gabriel-eb <gabo_espburg@hotmail.com>
2024-06-04 21:13:23 -0400

    Change to grid system

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,31 @@
 use bevy::prelude::*;
 
 const SNAKE_HEAD_COLOR: Color = Color::rgb(0.7, 0.7, 0.7);
-#[warn(dead_code)]
-const SNAKE_BODY_COLOR: Color = Color::rgb(0.6, 0.6, 0.6);
-#[warn(dead_code)]
-const FOOD_COLOR: Color = Color::rgb(0.3, 0.8, 0.5);
+// const SNAKE_BODY_COLOR: Color = Color::rgb(0.6, 0.6, 0.6);
+// const FOOD_COLOR: Color = Color::rgb(0.3, 0.8, 0.5);
+const ARENA_WIDTH: u32 = 10;
+const ARENA_HEIGHT: u32 = 10;
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins)
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                title: "SssssSnake!".into(),
+                name: Some("bevy.app".into()),
+                resolution: (500., 500.).into(),
+                // Tells wasm not to override default event handling, like F5, Ctrl+R etc.
+                prevent_default_event_handling: false,
+                enabled_buttons: bevy::window::EnabledButtons {
+                    maximize: false,
+                    ..Default::default()
+                },
+                ..default()
+            }),
+            ..default()
+        }))
         .add_systems(Startup, (setup_camera, spawn_snake))
+        .add_systems(Update, snake_movement)
+        .add_systems(PostUpdate, (position_translation, size_scailing))
         .run();
 }
 
@@ -42,6 +58,74 @@ fn spawn_snake(mut commands: Commands) {
             },
             ..default()
         },
+        Position { x: 3, y: 3 },
+        Size::square(0.8),
         SnakeHead,
     ));
+}
+
+fn snake_movement(
+    mut query: Query<&mut Position, With<SnakeHead>>,
+    keyboard: Res<ButtonInput<KeyCode>>,
+) {
+    for mut pos in &mut query {
+        if keyboard.pressed(KeyCode::ArrowUp) || keyboard.pressed(KeyCode::KeyW) {
+            pos.y += 1;
+        }
+        if keyboard.pressed(KeyCode::ArrowDown) || keyboard.pressed(KeyCode::KeyS) {
+            pos.y -= 1;
+        }
+        if keyboard.pressed(KeyCode::ArrowRight) || keyboard.pressed(KeyCode::KeyD) {
+            pos.x += 1;
+        }
+        if keyboard.pressed(KeyCode::ArrowLeft) || keyboard.pressed(KeyCode::KeyA) {
+            pos.x -= 1;
+        }
+    }
+}
+
+#[derive(Component, Clone, Copy, PartialEq, Eq)]
+struct Position {
+    x: i32,
+    y: i32,
+}
+
+#[derive(Component)]
+struct Size {
+    width: f32,
+    height: f32,
+}
+impl Size {
+    pub fn square(x: f32) -> Self {
+        Self {
+            width: x,
+            height: x,
+        }
+    }
+}
+
+fn size_scailing(windows: Query<&Window>, mut query: Query<(&Size, &mut Transform)>) {
+    let win = windows.single();
+    for (sprite_size, mut transform) in &mut query {
+        transform.scale = Vec3::new(
+            sprite_size.width / ARENA_WIDTH as f32 * win.width(),
+            sprite_size.height / ARENA_HEIGHT as f32 * win.height(),
+            1.0,
+        );
+    }
+}
+
+fn position_translation(windows: Query<&Window>, mut q: Query<(&Position, &mut Transform)>) {
+    fn convert(pos: f32, bound_window: f32, bound_game: f32) -> f32 {
+        let tile_size = bound_window / bound_game;
+        pos / bound_game * bound_window - (bound_window / 2.) + (tile_size / 2.)
+    }
+    let window = windows.single();
+    for (pos, mut transform) in q.iter_mut() {
+        transform.translation = Vec3::new(
+            convert(pos.x as f32, window.width(), ARENA_WIDTH as f32),
+            convert(pos.y as f32, window.height(), ARENA_HEIGHT as f32),
+            0.0,
+        );
+    }
 }
