f3a8f5532021c9421e49b9f7a0b98e828a933e54
gabriel-eb <gabo_espburg@hotmail.com>
2024-06-05 23:10:15 -0400

    fix tail follows head

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use bevy::prelude::*;
 use rand::random;
 
 const SNAKE_HEAD_COLOR: Color = Color::rgb(0.7, 0.7, 0.7);
-// const SNAKE_BODY_COLOR: Color = Color::rgb(0.6, 0.6, 0.6);
+const SNAKE_BODY_COLOR: Color = Color::rgb(0.5, 0.5, 0.5);
 const FOOD_COLOR: Color = Color::rgb(0.3, 0.8, 0.5);
 const SET_ARENA_WIDTH: u32 = 10;
 const SET_ARENA_HEIGHT: u32 = 10;
@@ -12,7 +12,8 @@ const ARENA_HEIGHT: f32 = SET_ARENA_HEIGHT as f32;
 fn main() {
     App::new()
         .insert_resource(ClearColor(Color::rgb(0.09, 0.09, 0.09)))
-        .insert_resource(Time::<Fixed>::from_seconds(0.15))
+        .insert_resource(Time::<Fixed>::from_seconds(0.4))
+        .insert_resource(SnakeSegments::default())
         .add_plugins(DefaultPlugins.set(WindowPlugin {
             primary_window: Some(Window {
                 title: "SssssSnake!".into(),
@@ -53,36 +54,59 @@ struct SnakeHead {
     direction: Direction,
 }
 
-fn spawn_snake(mut commands: Commands) {
-    commands.spawn((
-        SpriteBundle {
-            sprite: Sprite {
-                color: SNAKE_HEAD_COLOR,
-                ..default()
-            },
-            transform: Transform {
-                scale: Vec3::new(10.0, 10.0, 10.0),
-                ..default()
-            },
-            ..default()
-        },
-        Position { x: 3, y: 3 },
-        Size { side: 0.8 },
-        SnakeHead {
-            direction: Direction::Up,
-        },
-    ));
+fn spawn_snake(mut commands: Commands, mut segments: ResMut<SnakeSegments>) {
+    *segments = SnakeSegments(vec![
+        commands
+            .spawn((
+                SpriteBundle {
+                    sprite: Sprite {
+                        color: SNAKE_HEAD_COLOR,
+                        ..default()
+                    },
+                    transform: Transform {
+                        scale: Vec3::new(10.0, 10.0, 10.0),
+                        ..default()
+                    },
+                    ..default()
+                },
+                Position { x: 3, y: 3 },
+                Size { side: 0.8 },
+                SnakeHead {
+                    direction: Direction::Up,
+                },
+                SnakeSegment,
+            ))
+            .id(),
+        spawn_segment(commands, Position { x: 3, y: 2 }),
+    ]);
 }
 
-fn snake_movement(mut query: Query<(&mut Position, &SnakeHead)>) {
-    if let Some((mut snake_pos, snake)) = query.iter_mut().next() {
-        match &snake.direction {
-            Direction::Left => snake_pos.x -= 1,
-            Direction::Right => snake_pos.x += 1,
-            Direction::Up => snake_pos.y += 1,
-            Direction::Down => snake_pos.y -= 1,
+fn snake_movement(
+    segments: ResMut<SnakeSegments>,
+    mut snake: Query<(Entity, &SnakeHead)>,
+    mut positions: Query<&mut Position>,
+) {
+    if let Some((head_entity, head)) = snake.iter_mut().next() {
+        let mut head_pos = positions.get_mut(head_entity).unwrap();
+        match &head.direction {
+            Direction::Left => head_pos.x -= 1,
+            Direction::Right => head_pos.x += 1,
+            Direction::Up => head_pos.y += 1,
+            Direction::Down => head_pos.y -= 1,
         };
-    }
+
+        let body_positions = segments
+            .0
+            .iter()
+            .map(|e| *positions.get_mut(*e).unwrap())
+            .collect::<Vec<Position>>();
+        body_positions
+            .iter()
+            .zip(segments.0.iter().skip(1))
+            .for_each(|(pos, segment)| {
+                *positions.get_mut(*segment).unwrap() = *pos;
+            });
+    };
 }
 
 fn snake_movement_input(keyboard: Res<ButtonInput<KeyCode>>, mut query: Query<&mut SnakeHead>) {
@@ -180,4 +204,27 @@ impl Direction {
             Self::Down => Self::Up,
         }
     }
+}
+
+#[derive(Component)]
+struct SnakeSegment;
+
+#[derive(Resource, Default)]
+struct SnakeSegments(pub Vec<Entity>);
+
+fn spawn_segment(mut commands: Commands, position: Position) -> Entity {
+    commands
+        .spawn((
+            SpriteBundle {
+                sprite: Sprite {
+                    color: SNAKE_BODY_COLOR,
+                    ..default()
+                },
+                ..default()
+            },
+            position,
+            Size { side: 0.65 },
+            SnakeSegment,
+        ))
+        .id()
 }
