348f7235d388b34efadfeb42ae1d2d23b579e2c6
gabriel-eb <gabo_espburg@hotmail.com>
2024-06-05 22:21:54 -0400

    fix change movement to more snakelike sys

--- src/main.rs
+++ src/main.rs
@@ -6,13 +6,13 @@ const SNAKE_HEAD_COLOR: Color = Color::rgb(0.7, 0.7, 0.7);
 const FOOD_COLOR: Color = Color::rgb(0.3, 0.8, 0.5);
 const SET_ARENA_WIDTH: u32 = 10;
 const SET_ARENA_HEIGHT: u32 = 10;
-const ARENA_WIDTH:f32 = SET_ARENA_WIDTH as f32;
-const ARENA_HEIGHT:f32 = SET_ARENA_HEIGHT as f32;
+const ARENA_WIDTH: f32 = SET_ARENA_WIDTH as f32;
+const ARENA_HEIGHT: f32 = SET_ARENA_HEIGHT as f32;
 
 fn main() {
     App::new()
         .insert_resource(ClearColor(Color::rgb(0.09, 0.09, 0.09)))
-        .insert_resource(Time::<Fixed>::from_seconds(1.))
+        .insert_resource(Time::<Fixed>::from_seconds(0.15))
         .add_plugins(DefaultPlugins.set(WindowPlugin {
             primary_window: Some(Window {
                 title: "SssssSnake!".into(),
@@ -29,8 +29,8 @@ fn main() {
             ..default()
         }))
         .add_systems(Startup, (setup_camera, spawn_snake))
-        .add_systems(FixedUpdate, food_spawner)
-        .add_systems(Update, snake_movement)
+        .add_systems(FixedUpdate, (food_spawner, snake_movement))
+        .add_systems(Update, snake_movement_input.before(snake_movement))
         .add_systems(PostUpdate, (position_translation, size_scailing))
         .run();
 }
@@ -49,7 +49,9 @@ fn setup_camera(mut commands: Commands) {
 }
 
 #[derive(Component)]
-struct SnakeHead;
+struct SnakeHead {
+    direction: Direction,
+}
 
 fn spawn_snake(mut commands: Commands) {
     commands.spawn((
@@ -66,26 +68,39 @@ fn spawn_snake(mut commands: Commands) {
         },
         Position { x: 3, y: 3 },
         Size { side: 0.8 },
-        SnakeHead,
+        SnakeHead {
+            direction: Direction::Up,
+        },
     ));
 }
 
-fn snake_movement(
-    mut query: Query<&mut Position, With<SnakeHead>>,
-    keyboard: Res<ButtonInput<KeyCode>>,
-) {
-    for mut pos in &mut query {
-        if keyboard.pressed(KeyCode::ArrowUp) || keyboard.pressed(KeyCode::KeyW) {
-            pos.y += 1;
-        }
-        if keyboard.pressed(KeyCode::ArrowDown) || keyboard.pressed(KeyCode::KeyS) {
-            pos.y -= 1;
-        }
-        if keyboard.pressed(KeyCode::ArrowRight) || keyboard.pressed(KeyCode::KeyD) {
-            pos.x += 1;
-        }
-        if keyboard.pressed(KeyCode::ArrowLeft) || keyboard.pressed(KeyCode::KeyA) {
-            pos.x -= 1;
+fn snake_movement(mut query: Query<(&mut Position, &SnakeHead)>) {
+    if let Some((mut snake_pos, snake)) = query.iter_mut().next() {
+        match &snake.direction {
+            Direction::Left => snake_pos.x -= 1,
+            Direction::Right => snake_pos.x += 1,
+            Direction::Up => snake_pos.y += 1,
+            Direction::Down => snake_pos.y -= 1,
+        };
+    }
+}
+
+fn snake_movement_input(keyboard: Res<ButtonInput<KeyCode>>, mut query: Query<&mut SnakeHead>) {
+    if let Some(mut snake) = query.iter_mut().next() {
+        let dir: Direction =
+            if keyboard.pressed(KeyCode::ArrowUp) || keyboard.pressed(KeyCode::KeyW) {
+                Direction::Up
+            } else if keyboard.pressed(KeyCode::ArrowDown) || keyboard.pressed(KeyCode::KeyS) {
+                Direction::Down
+            } else if keyboard.pressed(KeyCode::ArrowRight) || keyboard.pressed(KeyCode::KeyD) {
+                Direction::Right
+            } else if keyboard.pressed(KeyCode::ArrowLeft) || keyboard.pressed(KeyCode::KeyA) {
+                Direction::Left
+            } else {
+                snake.direction
+            };
+        if dir != snake.direction.opposite() {
+            snake.direction = dir;
         }
     }
 }
@@ -146,4 +161,23 @@ fn food_spawner(mut commands: Commands) {
         Size { side: 0.5 },
         Food,
     ));
+}
+
+#[derive(PartialEq, Copy, Clone)]
+enum Direction {
+    Left,
+    Up,
+    Right,
+    Down,
+}
+
+impl Direction {
+    fn opposite(self) -> Self {
+        match self {
+            Self::Left => Self::Right,
+            Self::Right => Self::Left,
+            Self::Up => Self::Down,
+            Self::Down => Self::Up,
+        }
+    }
 }
